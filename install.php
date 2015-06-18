5a770916aaa4f49a8060162f903702b9ebc3b112
hubiao <hubiao@zhaodongxiaodeMacBook-Pro.local>
2015-06-18 16:50:06 +0800

    Add discuz 3.1

--- install.php
+++ install.php
@@ -12,7 +12,7 @@ switch(DISCUZ_VERSION) {
 		break;
 	case 'X3.1':
 		$dir = 'discuz_3_1';
-		$finish = install_x3_1();
+		$finish = install_x3_1($plugin_id);
 		break;
 	case 'X3.2':
 		$dir = 'discuz_3_2';
@@ -23,23 +23,35 @@ switch(DISCUZ_VERSION) {
 }
 
 function install_x3($plugin_id) {
-	$result = move_file($plugin_id);
+	$version = 'discuz_3_0';
+	$result = move_file($plugin_id, $version);
 	if(!$result) {
-		move_file($plugin_id, false);
+		move_file($plugin_id, $version, false);
+		return false;
+	}
+	return true;
+}
+function install_x3_1($plugin_id) {
+	$version = 'discuz_3_1';
+	return install($plugin_id, $version);
+}
+function install($plugin_id, $version) {
+	$result = move_file($plugin_id, $version);
+	if(!$result) {
+		move_file($plugin_id, $version, false);
 		return false;
 	}
 	return true;
 }
 
-function move_file($plugin_id,  $is_install = true) {
-	$install_dir = DISCUZ_ROOT . "source/plugin/$plugin_id/discuz_3_0/" . ($is_install ? 'install' : 'uninstall');
+function move_file($plugin_id,  $version, $is_install = true) {
+	$install_dir = DISCUZ_ROOT . "source/plugin/$plugin_id/$version/" . ($is_install ? 'install' : 'uninstall');
 	$changed = 0;
 	$changed += copy($install_dir . "/forum_attachment.php", DISCUZ_ROOT . "/source/module/forum/forum_attachment.php");
 	$changed += copy($install_dir . "/portal_attachment.php", DISCUZ_ROOT . "/source/module/portal/portal_attachment.php");
-	$changed += copy($install_dir . "/function_core.php", DISCUZ_ROOT . "/source/function/function_core.php");
 	$changed += copy($install_dir . "/discuz_ftp.php", DISCUZ_ROOT . "/source/class/discuz/discuz_ftp.php");
 	$changed += copy($install_dir . "/function_post.php", DISCUZ_ROOT . "/source/function/function_post.php");
-	if($changed == 5) {
+	if($changed == 4) {
 		return true;
 	} else {
 		return false;
