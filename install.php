311f0478c1894274ab2250e94e23eaaebb1a9ee4
gaara <sabakugaara@users.noreply.github.com>
2015-06-25 16:05:39 +0800

    取消防盗链稳定版

--- install.php
+++ install.php
@@ -4,12 +4,7 @@ if(!defined('IN_DISCUZ')) {
 }
 $plugin_id = 'upyun';
 include DISCUZ_ROOT . "source/plugin/$plugin_id/function_upyun.php";
+error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT & ~E_DEPRECATED);
 $version = upyun_get_discuz_version();
-$files = array(
-	DISCUZ_ROOT . "source/module/forum/forum_attachment.php",
-	DISCUZ_ROOT . "source/module/portal/portal_attachment.php",
-	DISCUZ_ROOT . "source/class/discuz/discuz_ftp.php",
-	DISCUZ_ROOT . "source/function/function_attachment.php",
-	DISCUZ_ROOT . "source/function/function_home.php",
-);
+$files = upyun_get_install_files();
 $finish = upyun_install($plugin_id, $version, $files);
