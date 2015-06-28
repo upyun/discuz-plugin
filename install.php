cf0bdaf67cb2bdfa04d1e9577c455aad3a0c9d61
hubiao <hubiao@ACA80165.ipt.aol.com>
2015-06-28 12:10:15 +0800

    Add 增加文件检测

--- install.php
+++ install.php
@@ -4,7 +4,6 @@ if(!defined('IN_DISCUZ')) {
 }
 $plugin_id = 'upyun';
 include DISCUZ_ROOT . "source/plugin/$plugin_id/function_upyun.php";
-error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT & ~E_DEPRECATED);
 $version = upyun_get_discuz_version();
 $files = upyun_get_install_files();
 $finish = upyun_install($plugin_id, $version, $files);
