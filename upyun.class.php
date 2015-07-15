c1287ae0da68d0babecb63ef2fbaeb6b6b68e74e
gaara <sabakugaara@users.noreply.github.com>
2015-07-15 10:34:41 +0800

    Update 将防盗链 Token 写入网站一级域名下

--- upyun.class.php
+++ upyun.class.php
@@ -6,6 +6,8 @@ include_once 'function_upyun.php';
 class plugin_upyun {
 	function global_header() {
 		global $_G;
-		setcookie('_upt', upyun_gen_sign());
+		//防盗链 token 写入用户网站的一级域名
+		$cookie_domain = substr($_SERVER['SERVER_NAME'], strpos($_SERVER['SERVER_NAME'], '.'));
+		setcookie('_upt', upyun_gen_sign(), $_SERVER['REQUEST_TIME'] + 180, '/', $cookie_domain);
 	}
 }
